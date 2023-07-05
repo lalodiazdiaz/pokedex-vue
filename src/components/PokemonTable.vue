3f5acf652878de5c529e2b607c6679d7e098b7d5
lalodiazdiaz <eduardodiazdiaz93@gmial.com>
2023-07-05 09:54:28 -0600

    set image on table

--- src/components/PokemonTable.vue
+++ src/components/PokemonTable.vue
@@ -5,13 +5,23 @@
       <thead>
         <tr>
           <th>Id</th>
+          <th>Image</th>
           <th>Name</th>
           <th>View</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(pokemon, index) in pokemonList" :key="index">
-          <th>{{ ++index }}</th>
+          <th>
+            {{ pokemon.id }}
+          </th>
+          <th class="w-25">
+            <img
+              class="w-25"
+              :src="pokemon.sprites.other.home.front_default"
+              alt=""
+            />
+          </th>
           <th>{{ pokemon.name }}</th>
           <th><button class="btn btn-primary">View more</button></th>
         </tr>
@@ -25,10 +35,20 @@ import { ref } from "vue";
 export default {
   name: "PokemonTable",
   setup() {
+    let pokemon = [];
     let pokemonList = ref([]);
-    fetch("https://pokeapi.co/api/v2/pokemon")
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
       .then((response) => response.json())
-      .then((data) => (pokemonList.value = data.results));
+      .then(
+        (data) => (
+          (pokemon = data.results),
+          pokemon.forEach((pokemonUrl) => {
+            fetch(pokemonUrl.url)
+              .then((response) => response.json())
+              .then((res) => pokemonList.value.push(res));
+          })
+        )
+      );
 
     return { pokemonList };
   },
